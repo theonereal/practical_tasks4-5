8f3b98f73b36f828dc55077cef3d3f9ac36d541c
theonereal <onereal@bk.ru>
2023-01-29 17:19:04 +0700

    Сделал задачу под номером 38 с пятого семинара

---
+++ Seminar5z3/Program.cs
@@ -0,0 +1,53 @@
+﻿/* 
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементами массива.
+[3 7 22 2 78] -> 76  
+*/
+
+int size = ReadInt("Введите размерность массива: ");
+int min = ReadInt("Введите минимальное число массива: ");
+int max = ReadInt("Введите максимальное число массива: ");
+int [] number = new int[size];
+
+FillArrayRandomNumber(number);
+PrintArray(number);
+
+int maxFillarray = number[0];
+int minFillarray = number[0];
+
+for (int i = 0; i < number.Length; i++)
+{
+    if (number[i] > maxFillarray)
+    {
+        maxFillarray = number[i];
+    }
+    if (number[i] < minFillarray)
+    {
+        minFillarray = number[i];
+    }
+}
+Console.WriteLine($"Разница между максимальным и минимальным числом = {maxFillarray - minFillarray}");
+
+
+void FillArrayRandomNumber(int [] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void PrintArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
