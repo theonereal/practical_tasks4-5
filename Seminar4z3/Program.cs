41b56026e6e47b0b2a02372f5fda5d0006bbfd6e
theonereal <onereal@bk.ru>
2023-01-29 15:26:17 +0700

    Сделал задачу под номером 29

---
+++ Seminar4z3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив 
+из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int lenArray = ReadInt("Введите длину массива: ");
+
+int [] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + "," + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
